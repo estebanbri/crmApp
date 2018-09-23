cda885ac0b23573a6ea5f1f07ed452bba0add256
Esteban Briceño <esteban_89_1@hotmail.com>
2018-09-23 04:37:03 -0300

    close and destroy ApplicationContext

--- src/main/java/com/crm/Test.java
+++ src/main/java/com/crm/Test.java
@@ -1,6 +1,5 @@
 package com.crm;
 
-import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 import com.crm.component.MiComponent;
@@ -11,14 +10,15 @@ public class Test {
 	public static void main(String[] args) {
 
         //Se produce un scan de Class-Path para encontrar AppConfiguration.class
-        ApplicationContext springContainer =
+		AnnotationConfigApplicationContext springContainer =
                     new AnnotationConfigApplicationContext(AppConfiguration.class);
 
         MiComponent cliente = springContainer.getBean("miComponent",MiComponent.class);
         System.out.println(cliente.getNombre());
         
-      
-	
+        springContainer.close();
+        
+ 
 	}
 
 }
