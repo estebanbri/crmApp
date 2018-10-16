70fae74cde3ae703f14c18426a5ada11abef243b
Esteban Briceño <esteban_89_1@hotmail.com>
2018-10-15 23:21:37 -0300

    + Swagger framework added

--- src/main/java/com/crm/rest/HomeController.java
+++ src/main/java/com/crm/rest/HomeController.java
@@ -7,8 +7,8 @@ import org.springframework.web.bind.annotation.GetMapping;
 public class HomeController {
 	
 	@GetMapping("/")
-	public String showMyPage() {
-		return "pagina-bienvenida";
+	public String swaggerUi() {
+		return "redirect:/swagger-ui.html";
 	}
 	
 }
